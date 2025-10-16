af063af4dfe4eef7acc61d371db0ac148984d59d
Kimmysoybad <6631501008@lamduan.mfu.ac.th>
2025-10-16 23:02:56 +0700

    initial commit

---
+++ assets/database/asset_borrowing.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 16, 2025 at 05:59 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `asset_borrowing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdata`
+--
+
+CREATE TABLE `userdata` (
+  `UserID` smallint(5) UNSIGNED NOT NULL,
+  `Role` enum('Borrower','Staff','Lender') NOT NULL,
+  `Name` varchar(20) NOT NULL,
+  `Username` varchar(20) NOT NULL,
+  `Password` smallint(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `userdata`
+--
+ALTER TABLE `userdata`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `userdata`
+--
+ALTER TABLE `userdata`
+  MODIFY `UserID` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ lib/identify_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IdentifyPage extends StatefulWidget {
+  const IdentifyPage({super.key});
+
+  @override
+  State<IdentifyPage> createState() => _IdentifyPageState();
+}
+
+class _IdentifyPageState extends State<IdentifyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
