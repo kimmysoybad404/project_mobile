805fe1db0f441c92c4546f6847bebd5190c41a0e
Dinozexe <Kittamat34@gmail.com>
2025-10-18 00:19:34 +0700

    Demo : Check Role BottomBar

--- lib/BottomBar.dart
+++ lib/BottomBar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
+import 'package:project_mobile/Borrower/home_page.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({super.key});
@@ -9,38 +10,55 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
+  int role = 1;
   final PageController _pageController = PageController(initialPage: 1);
   final NotchBottomBarController _controller = NotchBottomBarController(
     index: 1,
   );
 
-  final List<Widget> _pages = [
-    const Center(
-      child: Text(
-        'Request',
-        style: TextStyle(fontSize: 24, color: Colors.white),
-      ),
-    ),
-    const Center(
-      child: Text('Home', style: TextStyle(fontSize: 24, color: Colors.white)),
-    ),
-    const Center(
-      child: Text(
-        'Dashboard',
-        style: TextStyle(fontSize: 24, color: Colors.white),
-      ),
-    ),
+  final List<List<IconData>> roleIcons = [
+    [],
+    [Icons.receipt_long, Icons.home_filled, Icons.dashboard],
+    [Icons.receipt_long, Icons.home, Icons.dashboard],
+    [Icons.edit, Icons.home, Icons.dashboard],
   ];
+
+  List<Widget> CheckRole() {
+    switch (role) {
+      case 1:
+        return [const Scaffold(), const HomeBorrower(), const Scaffold()];
+      case 2:
+        return [const Scaffold(), const Scaffold(), const Scaffold()];
+      case 3:
+        return [const Scaffold(), const Scaffold(), const Scaffold()];
+      default:
+        return [const Scaffold()];
+    }
+  }
+
+  List<BottomBarItem> BottomBar() {
+    final icons = roleIcons[role];
+    return List.generate(
+      icons.length,
+      (index) => BottomBarItem(
+        inActiveItem: Icon(icons[index], color: Colors.blueGrey),
+        activeItem: Icon(icons[index], color: Colors.blueAccent),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final Pages = CheckRole();
+    final BottomBarNAJA = BottomBar();
+
     return SafeArea(
       child: Scaffold(
         backgroundColor: const Color.fromARGB(248, 64, 115, 255),
         extendBody: true,
         body: PageView(
           controller: _pageController,
-          children: _pages,
+          children: Pages,
           onPageChanged: (index) {
             _controller.index = index;
           },
@@ -52,21 +70,7 @@ class _BottomBarState extends State<BottomBar> {
           shadowElevation: 5,
           removeMargins: false,
           bottomBarHeight: 50,
-
-          bottomBarItems: const [
-            BottomBarItem(
-              inActiveItem: Icon(Icons.receipt_long, color: Colors.blueGrey),
-              activeItem: Icon(Icons.receipt_long, color: Colors.blueAccent),
-            ),
-            BottomBarItem(
-              inActiveItem: Icon(Icons.home_filled, color: Colors.blueGrey),
-              activeItem: Icon(Icons.home_filled, color: Colors.blueAccent),
-            ),
-            BottomBarItem(
-              inActiveItem: Icon(Icons.dashboard, color: Colors.blueGrey),
-              activeItem: Icon(Icons.dashboard, color: Colors.blueAccent),
-            ),
-          ],
+          bottomBarItems: BottomBarNAJA,
           onTap: (index) {
             _pageController.jumpToPage(index);
           },
