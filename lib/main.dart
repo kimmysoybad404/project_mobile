4d4f02736d2041e7c9f40cdb754550f7c7fbf9da
Dinozexe <Kittamat34@gmail.com>
2025-10-26 16:30:46 +0700

    Change : Home

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'Chewy',
       ),
-      home: BottomBar(),
+      home: IdentifyPage(),
     );
   }
 }
